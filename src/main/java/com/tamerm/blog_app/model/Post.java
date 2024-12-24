a64d3493d0cd7f2a69ed72d192905074336b9b4e
tamer.murtazaoglu <tamer.murtazaoglu@scalefocus.com>
2024-12-24 15:10:44 +0300

    Feature: Add Post and Tag entities
    - Created Post and Tag entity classes.
    - Defined the relationship between them (@ManyToMany).

---
+++ src/main/java/com/tamerm/blog_app/model/Post.java
@@ -0,0 +1,38 @@
+package com.tamerm.blog_app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Entity representing a blog post.
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String text;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "post_tags",
+            joinColumns = @JoinColumn(name = "post_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id", referencedColumnName = "id")
+    )
+    private Set<Tag> tags = new HashSet<>();
+}
