edde944adcd2ad1d07fe2b05fd80240bb2f287c8
tamer.murtazaoglu <tamer.murtazaoglu@scalefocus.com>
2024-12-24 16:11:26 +0300

    Feature: Create PostController - Added empty PostController class.

---
+++ src/main/java/com/tamerm/blog_app/controller/PostController.java
@@ -0,0 +1,13 @@
+package com.tamerm.blog_app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST controller for managing posts.
+ */
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+}
