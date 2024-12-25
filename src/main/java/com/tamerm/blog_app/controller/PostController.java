983aadec8624801f1fdd25a9f50ade3e27f17e09
tamer.murtazaoglu <tamer.murtazaoglu@scalefocus.com>
2024-12-25 16:42:19 +0300

    Feature: Get simplified list of all blog posts - Created an endpoint to retrieve a simplified list of all blog posts. - Each post in the list includes only the title and a short summary.

--- src/main/java/com/tamerm/blog_app/controller/PostController.java
+++ src/main/java/com/tamerm/blog_app/controller/PostController.java
@@ -1,16 +1,16 @@
 package com.tamerm.blog_app.controller;
 
 import com.tamerm.blog_app.dto.PostDTO;
+import com.tamerm.blog_app.dto.PostSummaryDTO;
 import com.tamerm.blog_app.request.CreatePostRequest;
 import com.tamerm.blog_app.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * REST controller for managing posts.
@@ -34,4 +34,14 @@ public class PostController {
         return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
     }
 
+    /**
+     * Get all post summaries.
+     *
+     * @return a list of post summaries
+     */
+    @GetMapping
+    public ResponseEntity<List<PostSummaryDTO>> getAllPostSummaries() {
+        List<PostSummaryDTO> posts = postService.getAllPosts();
+        return ResponseEntity.ok(posts);
+    }
 }
