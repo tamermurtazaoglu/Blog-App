91ae8d30feca6d9944f1f998dbc5ee2c6bfdb213
tamer.murtazaoglu <tamermurtazaoglu@gmail.com>
2024-12-25 11:18:41 +0300

    Feature: Create DTO layer and classes - Added PostDTO and PostSummaryDTO

---
+++ src/main/java/com/tamerm/blog_app/dto/PostDTO.java
@@ -0,0 +1,21 @@
+package com.tamerm.blog_app.dto;
+
+import com.tamerm.blog_app.model.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+/**
+ * Data Transfer Object for posts.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostDTO {
+    private Long id;
+    private String title;
+    private String text;
+    private Set<Tag> tags;
+}
