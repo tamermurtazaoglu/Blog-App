e74b3a07b8f3647c4a8fb0dca0e21181bb42d618
tamer.murtazaoglu <tamermurtazaoglu@gmail.com>
2024-12-25 16:42:19 +0300

    Feature: Get simplified list of all blog posts - Created an endpoint to retrieve a simplified list of all blog posts. - Each post in the list includes only the title and a short summary.

--- src/main/java/com/tamerm/blog_app/service/PostService.java
+++ src/main/java/com/tamerm/blog_app/service/PostService.java
@@ -1,8 +1,12 @@
 package com.tamerm.blog_app.service;
 
 import com.tamerm.blog_app.dto.PostDTO;
+import com.tamerm.blog_app.dto.PostSummaryDTO;
 import com.tamerm.blog_app.request.CreatePostRequest;
+
+import java.util.List;
 
 public interface PostService {
     PostDTO createPost(CreatePostRequest request);
+    List<PostSummaryDTO> getAllPosts();
 }
--- src/main/java/com/tamerm/blog_app/service/impl/PostServiceImpl.java
+++ src/main/java/com/tamerm/blog_app/service/impl/PostServiceImpl.java
@@ -1,6 +1,7 @@
 package com.tamerm.blog_app.service.impl;
 
 import com.tamerm.blog_app.dto.PostDTO;
+import com.tamerm.blog_app.dto.PostSummaryDTO;
 import com.tamerm.blog_app.exception.BadRequestException;
 import com.tamerm.blog_app.model.Post;
 import com.tamerm.blog_app.model.Tag;
@@ -12,7 +13,9 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Service implementation for managing posts.
@@ -47,6 +50,23 @@ public class PostServiceImpl implements PostService {
 
         Post savedPost = postRepository.save(post);
         return modelMapper.map(savedPost, PostDTO.class);
+    }
+
+    /**
+     * Retrieves all post summaries.
+     *
+     * @return a list of post summaries
+     */
+    @Override
+    public List<PostSummaryDTO> getAllPosts() {
+        return postRepository.findAll().stream()
+                .map(post -> new PostSummaryDTO(
+                        post.getTitle(),
+                        (post.getText() != null && post.getText().length() > 0)
+                                ? post.getText().substring(0, Math.min(post.getText().length(), 50)) + "..."
+                                : "No content available"
+                ))
+                .collect(Collectors.toList());
     }
 
 }
