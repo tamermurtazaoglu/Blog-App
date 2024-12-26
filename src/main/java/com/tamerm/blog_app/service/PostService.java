a0f6877c84cbbb3036c4a048f5f19cf5d01267b7
tamer.murtazaoglu <tamermurtazaoglu@gmail.com>
2024-12-26 10:35:08 +0300

    Feature: Update blog post - Implemented functionality to update the title, text and tags of an existing blog post. - Added validation to ensure valid title and text updates.

--- src/main/java/com/tamerm/blog_app/service/PostService.java
+++ src/main/java/com/tamerm/blog_app/service/PostService.java
@@ -3,10 +3,12 @@ package com.tamerm.blog_app.service;
 import com.tamerm.blog_app.dto.PostDTO;
 import com.tamerm.blog_app.dto.PostSummaryDTO;
 import com.tamerm.blog_app.request.CreatePostRequest;
+import com.tamerm.blog_app.request.UpdatePostRequest;
 
 import java.util.List;
 
 public interface PostService {
     PostDTO createPost(CreatePostRequest request);
     List<PostSummaryDTO> getAllPosts();
+    PostDTO updatePost(Long id, UpdatePostRequest request);
 }
